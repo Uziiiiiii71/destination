e52f7c57dcec34b9d39334e2a9f73bd045a65139
Uziiiiiii71 <fauzirifky00@gmail.com>
2025-07-17 01:11:33 +0700

    Delete routes directory

--- routes/destinations.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/destinationController');
-
-router.get('/', controller.getDestinations);
-router.post('/', controller.createDestination);
-
-module.exports = router;
